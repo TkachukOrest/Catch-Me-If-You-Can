69864735b0ea1fdfb7459aba20655d15bf0af3f3
Orest Tkachuk <transcontact62@gmail.com>
2016-02-14 23:38:26 +0200

    Changed Trip.cs access level.

--- CatchMe/CatchMe.Entities/Trip.cs
+++ CatchMe/CatchMe.Entities/Trip.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CatchMe.Entities
+﻿namespace CatchMe.Entities
 {
-    class Trip
+    public class Trip
     {
+        public WayInfo WayInfo { get; set; }
+        public TripInfo TripInfo { get; set; }
+        public VehicleInfo VehicleInfo { get; set; }        
     }
 }
